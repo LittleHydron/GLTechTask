6a0ee26237febd6e05ec93e4698d27e503d1d2fa
LittleHydron <little.hydron@gmail.com>
2023-07-22 16:11:38 +0300

    modified a searching library

--- searchLibrary/search.cpp
+++ searchLibrary/search.cpp
@@ -3,16 +3,19 @@
 #include <iostream>
 #include <queue>
 
-bool isPresent(const std::string &filename, const std::string &directory) {
+bool isPresent(const std::string &filename, const std::string &directory, bool* shouldStop) {
     std::queue < std::string > nextDirectories;
     nextDirectories.push(directory);
     while(!nextDirectories.empty()) {
+        if (*shouldStop) {
+            break;
+        }
         std::string currentDirectory = nextDirectories.front();
         nextDirectories.pop();
         try{
             for (const auto &entry: std::filesystem::directory_iterator(currentDirectory)) {
                 if (entry.path().filename().c_str() == filename) {
-                    std::cout << entry.path().c_str() << '\n';
+                    std::cout << "File found at: " << entry.path().c_str() << '\n';
                     return true;
                 }
                 if (entry.is_directory()) {
@@ -20,7 +23,7 @@ bool isPresent(const std::string &filename, const std::string &directory) {
                 }
             }
         } catch (std::filesystem::__cxx11::filesystem_error ex) {
-            std::cerr << ex.what() << '\n';
+            // std::cerr << ex.what() << '\n';
         }
     }
     return false;
--- searchLibrary/search.h
+++ searchLibrary/search.h
@@ -1,7 +1,8 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 #include <string>
+#include <atomic>
 
-bool isPresent(const std::string&, const std::string&);
+bool isPresent(const std::string&, const std::string&, bool*);
 
 #endif
